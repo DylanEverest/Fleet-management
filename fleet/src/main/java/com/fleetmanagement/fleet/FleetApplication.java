cca64ad85cb087c6d38bbb8b81d4d8cc6edb4b0b
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-01 15:37:38 +0300

    CommandLineRunner tested

--- fleet/src/main/java/com/fleetmanagement/fleet/FleetApplication.java
+++ fleet/src/main/java/com/fleetmanagement/fleet/FleetApplication.java
@@ -1,13 +1,20 @@
 package com.fleetmanagement.fleet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FleetApplication {
+public class FleetApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FleetApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello world");
+
 	}
 
 }
