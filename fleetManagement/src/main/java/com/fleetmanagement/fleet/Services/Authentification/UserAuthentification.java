5df588abea9e20b8ab2bc7efac31994770b9c2c6
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-02 14:41:33 +0300

    UserAuthentification

---
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Services/Authentification/UserAuthentification.java
@@ -0,0 +1,18 @@
+package com.fleetmanagement.fleet.Services.Authentification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fleetmanagement.fleet.Repositories.UserRepository;
+
+
+@Service
+public class UserAuthentification 
+{
+    @Autowired
+    UserRepository userRepository ;
+
+    
+
+    
+}
