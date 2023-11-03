b0ca3b1abcd1083961f77cd2c2dd9ad360821d84
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-03 22:16:36 +0300

    csrf disabled

--- fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/Security.java
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/Security.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,7 +25,8 @@ public class Security {
     public SecurityFilterChain filterChain ( HttpSecurity http) throws Exception
     {
          http
-        .csrf().disable()
+        .csrf(AbstractHttpConfigurer::disable)
+
         .authorizeHttpRequests(
            auth -> {
                     auth.requestMatchers("/fleet/auth/register").permitAll();
