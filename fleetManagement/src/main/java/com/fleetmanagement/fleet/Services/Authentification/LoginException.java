88e7f7a072962bb149e00ed5ac6cdd04677ffba8
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-04 18:48:51 +0300

    login service

---
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Services/Authentification/LoginException.java
@@ -0,0 +1,12 @@
+package com.fleetmanagement.fleet.Services.Authentification;
+
+public class LoginException extends Exception
+{
+
+    public LoginException(String string) 
+    {
+        super(string);
+    }
+
+
+}
--- fleetManagement/src/main/java/com/fleetmanagement/fleet/Services/Authentification/UserAuthentification.java
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Services/Authentification/UserAuthentification.java
@@ -1,6 +1,9 @@
 package com.fleetmanagement.fleet.Services.Authentification;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.fleetmanagement.fleet.Entities.User.User;
@@ -13,14 +16,38 @@ public class UserAuthentification
     @Autowired
     private UserRepository userRepository ;
 
-    public User login(User user)
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public User login(User user) throws LoginException
     {
-        User found = userRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        Optional <User> found = userRepository.findByEmail(user.getEmail());
         
-        return found ;
+        try 
+        {
+            if (isPasswordValid(user, found.get().getPassword()) ) 
+            {
+                return found.get();
+            }
+            else
+            {
+                throw new LoginException("Invalid credentials");
+            }
+            
+        } 
+        catch (Exception e) {
+            throw new LoginException("Invalid credentials");
+        }
+        
+
 
     }
     
 
+    public boolean isPasswordValid(User user, String rawPassword) {
+        
+        return passwordEncoder.matches(rawPassword, user.getPassword());
+    }
     
 }
