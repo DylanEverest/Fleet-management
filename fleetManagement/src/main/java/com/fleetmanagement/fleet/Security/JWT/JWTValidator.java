f81519d1d780accb5ce6a77ded4f09a0ad24a879
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-04 17:32:46 +0300

    validator

--- fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/JWT/JWTValidator.java
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/JWT/JWTValidator.java
@@ -1,5 +1,47 @@
 package com.fleetmanagement.fleet.Security.JWT;
 
-public class JWTValidator {
-    
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTValidator 
+{
+
+    @Value("${jwt.secret}") 
+    private String secretKey;
+
+
+    public Claims validateToken(String token) 
+    {
+        try 
+        {
+            Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+            if (isTokenExpired(claims.getExpiration())) 
+            {
+                return null; // Le token a expiré
+            }
+
+            return claims;
+        } 
+        catch (ExpiredJwtException | MalformedJwtException |  SignatureException e) {
+            return null; // La validation a échoué
+        }
+    }
+
+    private boolean isTokenExpired(Date expiration) {
+        return expiration.before(new Date());
+    }
 }
