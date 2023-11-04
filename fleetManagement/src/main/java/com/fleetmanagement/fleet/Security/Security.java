6b2aaaa1dda6e681d1ec2cdc52d95890ab8ca605
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-04 18:17:57 +0300

    passwordEncoder bean

--- fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/Security.java
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/Security.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -38,6 +39,12 @@ public class Security {
     }
 
 
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder ()
+    {
+        return new BCryptPasswordEncoder() ;
+    }
 
 
 
