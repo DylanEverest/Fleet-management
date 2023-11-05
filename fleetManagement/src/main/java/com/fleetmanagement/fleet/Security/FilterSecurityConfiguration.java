ce89014bd99bcf2dc0e74ef28ff9275dd3248327
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-05 16:50:11 +0300

    refacto

---
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/FilterSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.fleetmanagement.fleet.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.fleetmanagement.fleet.Security.JWT.Filter.JWTAuthorizationFilter;
+
+public class FilterSecurityConfiguration 
+{
+    @Autowired
+    private JWTAuthorizationFilter jwtAuthorizationFilter;
+
+
+    @Bean
+    public FilterRegistrationBean<JWTAuthorizationFilter> filterConfiguration(){
+        FilterRegistrationBean<JWTAuthorizationFilter> registrationBean 
+              = new FilterRegistrationBean<>();
+            
+        registrationBean.setFilter(jwtAuthorizationFilter);
+     
+        registrationBean.addUrlPatterns("/fleet/car", "/fleet/car/*");
+        
+        registrationBean.setOrder(1);
+
+        return registrationBean;    
+    }
+}
--- fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/Security.java
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/Security/Security.java
@@ -1,7 +1,5 @@
 package com.fleetmanagement.fleet.Security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +9,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.fleetmanagement.fleet.Security.JWT.Filter.JWTAuthorizationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class Security 
 {
-    @Autowired
-    private JWTAuthorizationFilter jwtAuthorizationFilter;
-
     
     @Bean
     @SuppressWarnings("removal")
@@ -35,9 +29,11 @@ public class Security
         // routes
         .authorizeHttpRequests(
            auth -> {
-                        // we authorize any request but filter will manage the authentification
-                        // and authorization for different request
-                        auth.anyRequest().permitAll() ;
+                    // we authorize any request but FilterSecurityConfiguration
+                    // will manage the authentification
+                    // and authorization for different request
+
+                    auth.anyRequest().permitAll() ;
                    }
            ) ;
 
@@ -52,21 +48,5 @@ public class Security
     {
         return new BCryptPasswordEncoder() ;
     }
-
-    @Bean
-    public FilterRegistrationBean<JWTAuthorizationFilter> filterConfiguration(){
-        FilterRegistrationBean<JWTAuthorizationFilter> registrationBean 
-              = new FilterRegistrationBean<>();
-            
-        registrationBean.setFilter(jwtAuthorizationFilter);
-     
-        registrationBean.addUrlPatterns("/fleet/car", "/fleet/car/*");
-        
-        registrationBean.setOrder(1);
-
-        return registrationBean;    
-    }
-
-
 
 }
