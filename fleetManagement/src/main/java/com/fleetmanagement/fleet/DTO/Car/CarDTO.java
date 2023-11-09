7da23caaac17d2b31491f09c8eb10c718695a788
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-09 10:52:24 +0300

    refacto DTO

--- fleetManagement/src/main/java/com/fleetmanagement/fleet/DTO/Car/CarDTO.java
+++ fleetManagement/src/main/java/com/fleetmanagement/fleet/DTO/Car/CarDTO.java
@@ -1,6 +1,5 @@
 package com.fleetmanagement.fleet.DTO.Car;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
